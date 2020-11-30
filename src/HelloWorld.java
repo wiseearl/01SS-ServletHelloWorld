f1df0b1c778dcdbb653dc4ea03193066b840a1c4
MDS <wiseearl@yahoo.com.tw>
2020-11-30 16:20:01 +0800

    first content

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloWorld
+ * Servlet implementation class HelloWorld,Test GitHut Desktop
  */
 @WebServlet(
 		urlPatterns = { 
